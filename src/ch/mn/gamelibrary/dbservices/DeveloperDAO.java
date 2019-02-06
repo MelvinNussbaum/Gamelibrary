dd9ca48391b89ad69169dd231ca3be708ebdf752
MelvinNussbaum <nussbaum.melvin@gmail.com>
2019-02-06 09:31:17 +0100

    created the DAO classes

---
+++ src/ch/mn/gamelibrary/dbservices/DeveloperDAO.java
@@ -0,0 +1,51 @@
+/******************************************************************************
+ *
+ * [ DeveloperDAO.java ]
+ *
+ * COPYRIGHT (c) 2002 - 2019 by Allianz-Suisse, Zürich, Switzerland.
+ * All rights reserved. This material contains unpublished, copyrighted
+ * work including confidential and proprietary information of Allianz-Suisse.
+ *
+ ******************************************************************************/
+package ch.mn.gamelibrary.dbservices;
+
+import ch.mn.gamelibrary.model.DBObject;
+
+public class DeveloperDAO implements IDAO {
+
+    @Override
+    public void create() {
+
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public DBObject read(DBObject dbObject) {
+
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void update(DBObject dbObject) {
+
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void delete(DBObject dbObject) {
+
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public DBObject[] readAll() {
+
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
