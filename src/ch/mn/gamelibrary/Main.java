6ac2ca5c6dbbafb0dd54863a62baac589bc126a1
MelvinNussbaum <nussbaum.melvin@gmail.com>
2019-02-07 09:18:48 +0100

    Added a static final String for Persistence Unit clarification

--- src/ch/mn/gamelibrary/Main.java
+++ src/ch/mn/gamelibrary/Main.java
@@ -9,12 +9,21 @@
  ******************************************************************************/
 package ch.mn.gamelibrary;
 
+import ch.mn.gamelibrary.dbservices.GameDAO;
+import ch.mn.gamelibrary.dbservices.PublisherDAO;
+
 public class Main {
 
     public static final String DB_SCHEMA = "Gamelibrary";
 
+    public static final String PERSISTENCE_UNIT = "PU_Eclipselink";
+
     public static void main(String[] args) {
 
+        GameDAO gameDAO = new GameDAO();
+        PublisherDAO publisherDao = new PublisherDAO();
+        gameDAO.create();
+        publisherDao.create();
     }
 
 }
