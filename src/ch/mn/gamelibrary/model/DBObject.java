598dacb0d20b7636f19814fa44eada3acd17fc88
MelvinNussbaum <nussbaum.melvin@gmail.com>
2019-02-07 09:13:29 +0100

    removed @Table annotation since it was senseless and changed the Generation strategy to IDENTITY

--- src/ch/mn/gamelibrary/model/DBObject.java
+++ src/ch/mn/gamelibrary/model/DBObject.java
@@ -13,16 +13,12 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
-import javax.persistence.Table;
-
-import ch.mn.gamelibrary.Main;
 
 @MappedSuperclass
-@Table(schema = Main.DB_SCHEMA)
 public abstract class DBObject {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     public DBObject() {
