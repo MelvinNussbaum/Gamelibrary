74becbbd134619661c2b9bea8dae23e9febbb197
MelvinNussbaum <nussbaum.melvin@gmail.com>
2019-02-12 12:22:49 +0100

    implemented ABstractDAO

--- src/ch/mn/gamelibrary/dbservices/AbstractDAO.java
+++ src/ch/mn/gamelibrary/dbservices/AbstractDAO.java
@@ -9,6 +9,9 @@
  ******************************************************************************/
 package ch.mn.gamelibrary.dbservices;
 
+import java.lang.reflect.ParameterizedType;
+
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
@@ -17,15 +20,67 @@ import ch.mn.gamelibrary.model.DBObject;
 
 public abstract class AbstractDAO<T extends DBObject> implements IDAO<T> {
 
-    protected EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory(Main.PERSISTENCE_UNIT);
+    protected EntityManagerFactory factory;
 
-    public EntityManagerFactory getEntityManagerFactory() {
+    protected EntityManager em;
 
-        return entityManagerFactory;
+    protected Class<T> entityClass;
+
+    public AbstractDAO() {
+        super();
+        factory = Persistence.createEntityManagerFactory(Main.PERSISTENCE_UNIT);
+        ParameterizedType genericSuperclass = (ParameterizedType) getClass().getGenericSuperclass();
+        this.entityClass = (Class<T>) genericSuperclass.getActualTypeArguments()[0];
     }
 
-    public void setEntityManagerFactory(EntityManagerFactory entityManagerFactory) {
+    private void before() {
 
-        this.entityManagerFactory = entityManagerFactory;
+        em = factory.createEntityManager();
+        em.getTransaction().begin();
     }
+
+    private void after() {
+
+        em.getTransaction().commit();
+        em.close();
+    }
+
+    @Override
+    public void persist(T dbObject) {
+
+        before();
+        em.persist(dbObject);
+        after();
+    }
+
+    @Override
+    public T retrieve(T dbObject) {
+
+        return em.find(entityClass, dbObject.getId());
+    }
+
+    @Override
+    public void update(T dbObject) {
+
+        before();
+        delete(dbObject);
+        em.persist(dbObject);
+        after();
+    }
+
+    @Override
+    public void delete(T dbObject) {
+
+        before();
+        em.remove(retrieve(dbObject));
+        after();
+    }
+
+    @Override
+    public T[] readAll() {
+
+        // TODO Auto-generated method stub
+        return null;
+    }
+
 }
