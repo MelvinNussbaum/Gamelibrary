7ba2f73a2c5a8f6c96860557db0ac492025abdc6
MelvinNussbaum <nussbaum.melvin@gmail.com>
2019-02-14 15:04:11 +0100

    added new model Genre.java

---
+++ src/ch/mn/gamelibrary/model/Genre.java
@@ -0,0 +1,67 @@
+/******************************************************************************
+ *
+ * [ Genre.java ]
+ *
+ * COPYRIGHT (c) 2002 - 2019 by Allianz-Suisse, Zürich, Switzerland.
+ * All rights reserved. This material contains unpublished, copyrighted
+ * work including confidential and proprietary information of Allianz-Suisse.
+ *
+ ******************************************************************************/
+package ch.mn.gamelibrary.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+
+/**
+ *
+ */
+@Entity
+public class Genre extends DBEntity implements Serializable {
+
+    private String name;
+
+    public Genre() {
+
+    }
+
+    public Genre(String name) {
+        super();
+        this.name = name;
+    }
+
+    public String getName() {
+
+        return name;
+    }
+
+    public void setName(String name) {
+
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+
+        String toString;
+        toString = "\n" + name;
+        return toString;
+    }
+
+    @Override
+    public int hashCode() {
+
+        return (int) (name.hashCode() + id);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+
+        if (obj == null)
+            return false;
+        if (!(obj instanceof Genre))
+            return false;
+        return this.hashCode() == obj.hashCode();
+    }
+
+}
