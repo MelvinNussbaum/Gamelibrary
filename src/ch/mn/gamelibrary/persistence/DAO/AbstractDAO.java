eac4a562760f613db6eb9fb33c79c983bf586f28
MelvinNussbaum <nussbaum.melvin@gmail.com>
2019-02-14 13:08:41 +0100

    removed everything witch wasnt an atomic db transaction (CRUD)

--- src/ch/mn/gamelibrary/persistence/DAO/AbstractDAO.java
+++ src/ch/mn/gamelibrary/persistence/DAO/AbstractDAO.java
@@ -7,92 +7,47 @@
  * work including confidential and proprietary information of Allianz-Suisse.
  *
  ******************************************************************************/
-package ch.mn.gamelibrary.dbservices;
+package ch.mn.gamelibrary.persistence.DAO;
 
 import java.lang.reflect.ParameterizedType;
-import java.util.List;
 
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-import javax.persistence.TypedQuery;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-
-import ch.mn.gamelibrary.Main;
 
 public abstract class AbstractDAO<T, K> implements IDAO<T, K> {
-
-    protected EntityManagerFactory factory;
 
     protected EntityManager em;
 
     protected Class<T> entityClass;
 
-    public AbstractDAO() {
+    @SuppressWarnings("unchecked")
+    public AbstractDAO(EntityManager em) {
         super();
-        factory = Persistence.createEntityManagerFactory(Main.PERSISTENCE_UNIT_ECLIPSELINK);
         ParameterizedType genericSuperclass = (ParameterizedType) getClass().getGenericSuperclass();
         this.entityClass = (Class<T>) genericSuperclass.getActualTypeArguments()[0];
-    }
-
-    protected void before() {
-
-        em = factory.createEntityManager();
-        em.getTransaction().begin();
-    }
-
-    protected void after() {
-
-        em.getTransaction().commit();
-        em.close();
+        this.em = em;
     }
 
     @Override
     public void persist(T dbObject) {
 
-        before();
         em.persist(dbObject);
-        after();
     }
 
     @Override
     public T retrieve(K primaryKey) {
 
-        em = factory.createEntityManager();
-        T dbObject = em.find(entityClass, primaryKey);
-        em.close();
-        return dbObject;
+        return em.find(entityClass, primaryKey);
     }
 
     @Override
     public void update(T dbObject) {
 
-        before();
         em.merge(dbObject);
-        after();
     }
 
     @Override
     public void delete(T dbObject) {
 
-        before();
-        T mergedObject = em.merge(dbObject);
-        em.remove(mergedObject);
-        after();
+        em.remove(em.merge(dbObject));
     }
-
-    @Override
-    public List<T> readAll() {
-
-        em = factory.createEntityManager();
-        CriteriaBuilder cb = em.getCriteriaBuilder();
-        CriteriaQuery<T> cq = cb.createQuery(entityClass);
-        CriteriaQuery<T> getAll = cq.select(cq.from(entityClass));
-        TypedQuery<T> query = em.createQuery(getAll);
-        List<T> results = query.getResultList();
-        em.close();
-        return results;
-    }
-
 }
