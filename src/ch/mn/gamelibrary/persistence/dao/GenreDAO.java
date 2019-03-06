fa4481918afd2421a625c1b41bd654e97d1f058f
MelvinNussbaum <nussbaum.melvin@gmail.com>
2019-03-06 14:41:40 +0100

    added findByName()

--- src/ch/mn/gamelibrary/persistence/dao/GenreDAO.java
+++ src/ch/mn/gamelibrary/persistence/dao/GenreDAO.java
@@ -9,14 +9,43 @@
  ******************************************************************************/
 package ch.mn.gamelibrary.persistence.dao;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.ParameterExpression;
+import javax.persistence.criteria.Root;
 
 import ch.mn.gamelibrary.model.Genre;
 
-public class GenreDAO extends AbstractDAO<Genre, Long> {
+public class GenreDAO extends AbstractDAO<Genre> {
 
     public GenreDAO(EntityManager em) {
         super(em);
+    }
+
+    public Genre findByName(String genre) {
+
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+        ParameterExpression<String> params = cb.parameter(String.class);
+        CriteriaQuery<Genre> criteria = cb.createQuery(Genre.class);
+        Root<Genre> root = criteria.from(Genre.class);
+
+        criteria.select(root);
+        criteria.where(cb.equal(root.get("name"), params));
+
+        TypedQuery<Genre> query = em.createQuery(criteria);
+        query.setParameter(params, genre);
+
+        List<Genre> queryResult = query.getResultList();
+
+        if (queryResult.isEmpty()) {
+            return null;
+        }
+
+        return queryResult.get(0);
     }
 
 }
