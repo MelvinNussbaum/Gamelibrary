2ccfb3c1dfe0e6800a538fb747b25c9cbf4bf5d1
MelvinNussbaum <nussbaum.melvin@gmail.com>
2019-03-13 13:19:17 +0100

    basically redone

--- src/ch/mn/gamelibrary/controller/MainViewController.java
+++ src/ch/mn/gamelibrary/controller/MainViewController.java
@@ -9,10 +9,15 @@
  ******************************************************************************/
 package ch.mn.gamelibrary.controller;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+
 import ch.mn.gamelibrary.model.Game;
+import ch.mn.gamelibrary.model.Genre;
 import ch.mn.gamelibrary.persistence.service.GameService;
+import ch.mn.gamelibrary.utils.NumberFormat;
 import javafx.fxml.FXML;
-import javafx.geometry.Pos;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.image.Image;
@@ -20,11 +25,12 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.VBox;
-import javafx.scene.text.Font;
 
 public class MainViewController {
 
     private GameService service = new GameService();
+
+    private GamePanelController panelController = new GamePanelController(this);
 
     @FXML
     private BorderPane parent;
@@ -35,44 +41,102 @@ public class MainViewController {
     @FXML
     private FlowPane gamesContainer;
 
+    @FXML
+    private VBox detailContainer;
+
+    @FXML
+    private ImageView detailImageView;
+
+    @FXML
+    private Label detailGameTitle;
+
+    @FXML
+    private Label detailGameGenres;
+
+    @FXML
+    private Label detailGameMetaScore;
+
+    @FXML
+    private Label detailGameUnitsSold;
+
+    @FXML
+    private Label detailDeveloperTitle;
+
+    @FXML
+    private Label detailDeveloperHQ;
+
+    @FXML
+    private Label detailDeveloperCEO;
+
+    @FXML
+    private Label detailPublisherTitle;
+
+    @FXML
+    private Label detailPublisherHQ;
+
+    @FXML
+    private Label detailPublisherCEO;
+
     public MainViewController() {
     }
 
     @FXML
     private void initialize() {
 
-        scrollPane.widthProperty()
-            .addListener((obs, newVal, oldVal) -> gamesContainer.setPrefWrapLength((double) newVal));
-
         for (Game game : service.readAll()) {
             createGamePanel(game);
         }
+
+        gamesContainer.prefWrapLengthProperty().bind(scrollPane.widthProperty());
     }
 
     private void createGamePanel(Game game) {
 
-        VBox gamePanel = new VBox();
-        gamePanel.setAlignment(Pos.TOP_CENTER);
-        gamePanel.setMaxWidth(120);
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../view/component/GamePanel.fxml"));
+            fxmlLoader.setController(panelController);
+            VBox gamePanel = fxmlLoader.load();
+            panelController.setGame(game);
 
-        Image image = new Image(getClass().getResourceAsStream("/assets/img/Video_Game_Cover_The_Last_of_Us.jpg"));
-        ImageView imgView = new ImageView(image);
-        imgView.setPreserveRatio(true);
-        imgView.setFitHeight(150);
-
-        Label title = new Label(game.getTitle());
-        Label pub = new Label(game.getPublisher().getName());
-        Label dev = new Label(game.getDeveloper().getName());
-        title.setFont(new Font(14));
-        pub.setFont(new Font(10));
-        dev.setFont(new Font(10));
-
-        gamePanel.getChildren().add(imgView);
-        gamePanel.getChildren().add(title);
-        gamePanel.getChildren().add(pub);
-        gamePanel.getChildren().add(dev);
-
-        gamesContainer.getChildren().add(gamePanel);
+            gamePanel.setOnMouseClicked(panelController);
+            gamesContainer.getChildren().add(gamePanel);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
+    public void fillDetailContainer(String gameTitle) {
+
+        Game game = null;
+        try {
+            game = service.findByName(gameTitle);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        StringBuilder genreString = new StringBuilder();
+        for (Genre genre : game.getGenres()) {
+            if (genreString.length() != 0) {
+                genreString.append(", ");
+            }
+            genreString.append(genre.toString());
+        }
+
+        ByteArrayInputStream bis = new ByteArrayInputStream(game.getCover());
+        detailImageView.setImage(new Image(bis));
+        detailGameTitle.setText(game.getName());
+        detailGameGenres.setText(genreString.toString());
+        detailGameMetaScore.setText("MetaScore: " + game.getMetaScore() + "/100");
+        detailGameUnitsSold.setText("Units sold: " + NumberFormat.format(game.getUnitsSold()));
+        detailPublisherTitle.setText(game.getPublisher().getName());
+        detailPublisherCEO.setText("CEO: " + game.getPublisher().getCeo());
+        detailPublisherHQ.setText("Headquarters: " + game.getPublisher().getHq());
+        detailDeveloperTitle.setText(game.getDeveloper().getName());
+        detailDeveloperCEO.setText("CEO: " + game.getDeveloper().getCeo());
+        detailDeveloperHQ.setText("Headquarters: " + game.getDeveloper().getHq());
+
+        if (!detailContainer.isVisible()) {
+            detailContainer.setVisible(true);
+        }
+    }
 }
