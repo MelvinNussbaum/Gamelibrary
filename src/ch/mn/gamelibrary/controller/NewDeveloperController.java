fa3fc31ece2f97a87ba53c03344568718bda90ba
MelvinNussbaum <nussbaum.melvin@gmail.com>
2019-03-20 14:27:37 +0100

    added and implemented generic NewProducerController.java

---
+++ src/ch/mn/gamelibrary/controller/NewDeveloperController.java
@@ -0,0 +1,20 @@
+/******************************************************************************
+ *
+ * [ NewDeveloperController.java ]
+ *
+ * COPYRIGHT (c) 2002 - 2019 by Allianz-Suisse, Zürich, Switzerland.
+ * All rights reserved. This material contains unpublished, copyrighted
+ * work including confidential and proprietary information of Allianz-Suisse.
+ *
+ ******************************************************************************/
+package ch.mn.gamelibrary.controller;
+
+import ch.mn.gamelibrary.model.Developer;
+
+public class NewDeveloperController extends NewProducerController<Developer> {
+
+    public NewDeveloperController() throws InstantiationException, IllegalAccessException {
+        super();
+    }
+
+}
